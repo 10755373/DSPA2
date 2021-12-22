194b18d255f07256ec63414fb71bfc6ca95de138
Alexandra Spruit <alexandra.spruit@hotmail.com>
2021-12-22 19:47:06 +0100

    navigation bar linking

--- run.py
+++ run.py
@@ -6,5 +6,25 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
+@app.route("/index.html")
+def home():
+    return render_template("index.html")
+
+@app.route("/gallery.html")
+def gallery():
+    return render_template("gallery.html")
+
+@app.route("/map.html")
+def map():
+    return render_template("map.html")
+
+@app.route("/upload.html")
+def upload():
+    return render_template("upload.html")
+
+@app.route("/ethics_paper.html")
+def ethics_paper():
+    return render_template("ethics_paper.html")
+
 if __name__ == "__main__":
     app.run()

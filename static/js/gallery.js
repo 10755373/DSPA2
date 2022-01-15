63ce93fd7d95b2fbb1946963d36e53a9954d101a
10755373 <rinusvangrunsven@gmail.com>
2022-01-15 15:20:22 +0100

    gallery live interaction with firebase storage

--- static/js/gallery.js
+++ static/js/gallery.js
@@ -22,103 +22,45 @@ const analytics = getAnalytics(app);
 const storage = getStorage();
 const storageRef = ref(storage, "Images/");
 
-var URLs = [];
-
-// Find all the prefixes and items.
-listAll(storageRef)
-    .then((res) => {
-        res.prefixes.forEach((folderRef) => {
-          // All the prefixes under listRef.
-          // You may call listAll() recursively on them.
-        });
-        res.items.forEach((itemRef) => {
-          // All the items under listRef.
-//          console.log("itemref: ", itemRef);
-          getDownloadURL(itemRef).then((url) => {
-            console.log("download url: " + url);
-            URLs.push(url);
-          })
-        });
-    }).catch((error) => {
-        console.log("error")
-    });
-
-console.log(URLs)
-
-var URLs = [
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142254/html9.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143104/html10.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142245/CSS8.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143101/CSS9.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142240/Bootstrap5.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143058/Bootstrap6.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142257/JavaScript2.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143106/JavaScript3.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142303/jquery.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143108/jquery4.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142309/php7.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142254/html9.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143104/html10.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142245/CSS8.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143101/CSS9.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142240/Bootstrap5.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143058/Bootstrap6.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142257/JavaScript2.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143106/JavaScript3.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142303/jquery.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143108/jquery4.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142309/php7.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142254/html9.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143104/html10.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142245/CSS8.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143101/CSS9.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142240/Bootstrap5.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143058/Bootstrap6.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142257/JavaScript2.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143106/JavaScript3.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142303/jquery.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143108/jquery4.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142309/php7.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142254/html9.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143104/html10.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142245/CSS8.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143101/CSS9.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142240/Bootstrap5.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143058/Bootstrap6.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142257/JavaScript2.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143106/JavaScript3.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142303/jquery.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143108/jquery4.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318142309/php7.png",
-  "https://media.geeksforgeeks.org/wp-content/uploads/20200318143112/php8.png"
-  ];
-var imgs = URLs.map(function(URL) {
-	var img = new Image();
-	img.src = URL;
-  img.setAttribute("data-lightbox", "galdatalight");
-  var idhtml = document.getElementsByClassName("grid")[0];
-  idhtml.appendChild(img);
-	return img;
-});
 
 const lightbox = document.createElement('div')
 lightbox.id = 'lightbox'
 document.body.appendChild(lightbox)
+listAll(storageRef).then(function(result){
+    console.log(result);
+    result.items.forEach(function(itemRef){
+      getDownloadURL(itemRef).then(function(url){
+            var idhtml = document.getElementsByClassName("gallery")[0];
+            	var img = new Image();
+	            img.src = url;
+              img.setAttribute("data-lightbox", "galdatalight");
+            idhtml.appendChild(img);
+        });
+    })
+}).catch(function(error){
+    console.log(error);
+});
 
-const images = document.querySelectorAll('img')
-images.forEach(image => {
-  image.addEventListener('click', e => {
-    lightbox.classList.add('active')
-    const img = document.createElement('img')
-    img.src = image.src
-    while (lightbox.firstChild) {
-      lightbox.removeChild(lightbox.firstChild)
-    }
-    lightbox.appendChild(img )
-  })
-})
 
-lightbox.addEventListener('click', e => {
-  if (e.target !== e.currentTarget) return
-  lightbox.classList.remove('active')
-})
+// The onclick part is not yet working!!
+// const lightbox = document.createElement('div')
+// lightbox.id = 'lightbox'
+// document.body.appendChild(lightbox)
+// var images = document.getElementById('myGallery0').querySelectorAll('img');
+// images.forEach(image => {
+//   image.addEventListener('click', e => {
+//     lightbox.classList.add('active')
+//     const img = document.createElement('img')
+//     img.src = image.src
+//     while (lightbox.firstChild) {
+//       lightbox.removeChild(lightbox.firstChild)
+//     }
+//     lightbox.appendChild(img )
+//   })
+// })
+
+// lightbox.addEventListener('click', e => {
+//   if (e.target !== e.currentTarget) return
+//   lightbox.classList.remove('active')
+// })
+
